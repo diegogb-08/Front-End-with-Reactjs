1426e15c31b86277fc79a6d36364493a86be0a12
keika <jesicamorocho.m@gmail.com>
2021-03-09 20:24:35 +0100

    adding description of clinic to home view

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+
 import Header from '../../components/Header/Header';
 import './Home.css';
+
 import Photo from '../../img/mainPhotoGirls.png'
+import Geo from '../../img/geo.png'
 
 
 
@@ -15,7 +18,17 @@ const Home = (props) => {
             <div className="mainPhoto">
             <img className="homePhoto" alt="working" src={Photo}/> 
             </div>
-            <div className="mainDescription">¿QUIENES SOMOS?</div>
+            <div className="mainDescription">
+                <h2>Bienvenidos a la clínica dental JessDieg</h2>
+                <h3>¿Quiénes Somos?</h3>
+                <p>Somos un equipo de profesionales cuidando su salud bucal.<br/><br/>
+                JessDieg es tu sitio de confianza donde ofrecemos la más alta calidad en servicios integrales de odontología.<br/><br/>
+                Contamos con todas las especialidades en el ámbito dental, utilizando las últimas tecnologías.
+                </p>
+                <h3>¿Dónde estamos?<img className="GeoImg" alt="localitation" src={Geo}/></h3>
+                <p>C/Almirante Cadarso,26<br/><br/>46021, Valencia.</p>
+                
+            </div>
             
         </div>
         
