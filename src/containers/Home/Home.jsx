856e35cf33292a1a6f92619b43935b0a3497e07d
keika <jesicamorocho.m@gmail.com>
2021-03-09 21:24:41 +0100

    adding div over photo to publicity message

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -16,8 +16,10 @@ const Home = (props) => {
         <div className="homeContainer">
             <Header/>
             <div className="mainPhoto">
-            <img className="homePhoto" alt="working" src={Photo}/> 
+                <div className="textPhoto"><p>Clinica JessDieg,</p><p>Cuidamos de tu :)</p></div>
+                <img className="homePhoto" alt="working" src={Photo}/> 
             </div>
+
             <div className="mainDescription">
                 <h2>Bienvenidos a la clínica dental JessDieg</h2>
                 <h3>¿Quiénes Somos?</h3>
@@ -26,10 +28,9 @@ const Home = (props) => {
                 Contamos con todas las especialidades en el ámbito dental, utilizando las últimas tecnologías.
                 </p>
                 <h3>¿Dónde estamos?<img className="GeoImg" alt="localitation" src={Geo}/></h3>
-                <p>C/Almirante Cadarso,26<br/><br/>46021, Valencia.</p>
-                
+                <p>C/Almirante Cadarso,26<br/><br/>46021, Valencia.</p>  
             </div>
-            
+
         </div>
         
     )
