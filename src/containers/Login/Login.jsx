aa7b8ea64ef5b6b4f582e8750cf4e99886d0faf0
keika <jesicamorocho.m@gmail.com>
2021-03-10 22:11:54 +0100

    refactoring login function

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -2,12 +2,15 @@ import React, {useState, useEffect} from 'react';
 import Input from '../../components/Input/Input'
 import Submit from '../../components/Submit/Submit';
 import axios from 'axios';
-import {port, login} from '../../api/api';
+import {port, client, login} from '../../api/api'; 
+import Header from '../../components/Header/Header';
+/* import {useHistory} from 'react-router-dom';  */
 
 
 
 
 const Login = () => {
+
 
     const [dataLogin, setLogin] = useState({
         
@@ -25,15 +28,20 @@ const Login = () => {
     },[]);
 
     const logeame = async () => {
-        let result = await axios.post(port+login, dataLogin)
-        console.log(result);
+       
+
+       
+        let result = await axios.post(port+client+login, dataLogin)
+        console.log(result); 
     }
 
     return(
-        <div>
+        <div className='loginContainer'>
+            {/* <Header/> */}
+            <div className='designDiv'></div>
             <Input type='email' name='email' title='Email' lenght='30' onChange={handleState}/>
-             <Input type='password' name='password' title='Password' lenght='16' onChange={handleState}/>
-            <Submit type='submit' name='submit'title='logeame' onClick={() => logeame()}/>
+            <Input type='password' name='password' title='Password' lenght='16' onChange={handleState}/>
+            <Submit type='submit' name='submit' title='logeame' onClick={() => logeame()}/>
         </div>
     )
 
