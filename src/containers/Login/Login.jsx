18f11a91d660000ed89bb37677035d3f0ea537d0
keika <jesicamorocho.m@gmail.com>
2021-03-11 13:46:11 +0100

    adding style to login

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,11 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState/* , useEffect */} from 'react';
 import Input from '../../components/Input/Input'
 import Submit from '../../components/Submit/Submit';
 import axios from 'axios';
 import {port, client, login} from '../../api/api'; 
 import Header from '../../components/Header/Header';
 import './Login.css';
-/* import {useHistory} from 'react-router-dom';  */
+import {useHistory} from 'react-router-dom';  
+import Logo from '../../img/logoclinic.png'
 
 
 
@@ -13,34 +14,58 @@ import './Login.css';
 
 const Login = () => {
 
+    let history = useHistory();
+
 
     const [dataLogin, setLogin] = useState({
         
         email: '',
         password: ''
        
-    })
+    });
+
+    const [message, setMessage] = useState('')
 
     const handleState = (event) => {
         setLogin({...dataLogin, [event.target.name]: event.target.type === "number" ? +event.target.value : event.target.value});
     };
-
+/* 
     useEffect(() => {
 
-    },[]);
+    },[]); */
 
     const logeame = async () => {
+        try {
         let result = await axios.post(port+client+login, dataLogin)
-        console.log(result); 
+            if(result) {
+                localStorage.setItem("loginUser", JSON.stringify(result.data));
+                return history.push(`/user`) 
+            }
+        } catch(error) {
+            setMessage('Email or password not found');
+        }
     }
 
     return(
         <div className='loginContainer'>
-             <Header/> 
-            <div className='designDiv'></div>
-            <Input type='email' name='email' title='Email' lenght='30' onChange={handleState}/>
-            <Input type='password' name='password' title='Password' lenght='16' onChange={handleState}/>
-            <Submit type='submit' name='submit' title='logeame' onClick={() => logeame()}/>
+            <Header/> 
+            <h1>Log In!</h1>
+            <div className='cardLoginContainer'>
+                <div className='cardLogin'>
+                    
+                        <img className="loginLogoImg" alt="logo" src={Logo}/> 
+                    
+                    <Input type='email' name='email' title='Email' lenght='30' onChange={handleState}/>
+                    <Input type='password' name='password' title='Password' lenght='16' onChange={handleState}/>
+
+                </div>
+
+                <div className="messageLogin">
+                     <div className="messageErrorLogin">{message}</div>
+                     <Submit type='submit' name='submit' title='logeame' onClick={() => logeame()}/>
+                </div>
+
+            </div>
         </div>
     )
 
