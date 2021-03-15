06610ed588631068b58eda31245907fae9cae28d
Diego <d.garciabrisa@gmail.com>
2021-03-15 23:35:47 +0100

    react-bootstrap uninstalled

--- src/containers/ModalForm/PopUpModal.jsx
+++ src/containers/ModalForm/PopUpModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Modal from 'react-bootstrap/Modal'
+
 import Input from '../../components/Input/Input';
 import Submit from '../../components/Submit/Submit';
 import './ModalForm.css'
@@ -10,17 +10,17 @@ function PopUpModal(props) {
     }
 
     return (
-      <Modal
-        {...props}
-        // aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Update your profile!
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
+    //   <Modal
+    //     {...props}
+    //     // aria-labelledby="contained-modal-title-vcenter"
+    //     centered
+    //   >
+    //     <Modal.Header>
+    //       <Modal.Title id="contained-modal-title-vcenter">
+    //         Update your profile!
+    //       </Modal.Title>
+    //     </Modal.Header>
+    //     <Modal.Body>
             <div className="formModal">
                 <Input type='text' name='fullName' title='Full Name' lenght='30'/>
                 <Input type='text' name='userName' title='User Name' lenght='30' />
@@ -28,14 +28,14 @@ function PopUpModal(props) {
                 <Input type='number' name='phoneNumber' title='Phone Number' lenght='12' />
                 <Input type='text' name='address' title='Full Address' lenght='250'/>
             </div>
-        </Modal.Body>
-        <Modal.Footer>
-            <div className="onHide" onClick={props.onHide}>
-                <Submit type='submit' name='update' onClick={()=>submit()} title='Update Profile' />
-            </div>
+    //     </Modal.Body>
+    //     <Modal.Footer>
+    //         <div className="onHide" onClick={props.onHide}>
+    //             <Submit type='submit' name='update' onClick={()=>submit()} title='Update Profile' />
+    //         </div>
        
-        </Modal.Footer>
-      </Modal>
+    //     </Modal.Footer>
+    //   </Modal>
     );
 }
 
